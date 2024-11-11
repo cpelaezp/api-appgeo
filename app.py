e60be578c4bd5ed4cb097a617d8314d47de4e898
Cesar Josseph Peláez Pérez <cpelaezp@gmail.com>
2024-11-10 19:13:49 -0500

    ajustes

--- app.py
+++ app.py
@@ -83,4 +83,6 @@ if __name__ == '__main__':
     CORS(app)
 
     #app.run(debug=True)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    ## app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
